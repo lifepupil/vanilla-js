8161f512efff6e897aacb78af37d2b05e49e9519
Christian Richard <lifepupil@gmail.com>
2015-04-13 15:42:07 -0700

    new stuff regarding prototype chains

--- objects.js
+++ objects.js
@@ -1,31 +1,41 @@
-var dog = {};
-dog.name = 'fido';
-dog.age = 3;
+// var dog = {};
+// dog.name = 'fido';
+// dog.age = 3;
+//
+// // can create an object like this too
+// var cat = {name:'fluffy', age:5};
+// console.log(dog, cat);
+//
+// // give the cat some toys
+// cat.toys = ['fluff', 'scratch post', 'mouse'];
+// console.log(cat);
+//
+// console.log('')
+// console.log('OMG I forgot to give cat another toy');
+//
+// cat.toys.push('small box');
+// console.log(cat);
+// console.log('cat doesnt like scratch post so remove with splice');
+// cat.toys.splice(1,1);
+// console.log(cat);
+//
+// console.log('');
+// console.log('creating a constructor for Cat object');
 
-// can create an object like this too
-var cat = {name:'fluffy', age:5};
-console.log(dog, cat);
-
-// give the cat some toys
-cat.toys = ['fluff', 'scratch post', 'mouse'];
-console.log(cat);
-
-console.log('')
-console.log('OMG I forgot to give cat another toy');
-
-cat.toys.push('small box');
-console.log(cat);
-console.log('cat doesnt like scratch post so remove with splice');
-cat.toys.splice(1,1);
-console.log(cat);
-
-console.log('');
-console.log('creating a constructor for Cat object');
 function Cat(name, age){
+  // the this. refers to the object
   this.name = name;
   this.age = age;
 }
+
+
+
+Cat.prototype.purr = function() {
+  console.log(this.name+' is purring')
+}
 // this creates new Cat object
- var fluffy = new Cat('Miss Sassypants', 3);
-// to verify that this code works
-console.log('fluffy', fluffy);
+var fluffy = new Cat('fluffy', 3);
+fluffy.purr();
+
+var molly = new Cat('molly', 5)
+molly.purr();
